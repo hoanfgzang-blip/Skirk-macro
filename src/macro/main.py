b65cbfec597969a61eea54c70dbd6efa5f2bbaca
hoanfgzang-blip <hoanfgzang@gmail.com>
2026-07-09 01:33:47 +0700

    Refactor macro functions and add FPS interpolation logic

--- src/macro/main.py
+++ src/macro/main.py
@@ -5,8 +5,229 @@ import subprocess
 import json
 import os
 import pynput
+import threading
+import keyboard
 
 mouse = pynput.mouse.Controller()
-keyboard = pynput.keyboard.Controller()
+inputkeyboard = pynput.keyboard.Controller()
 left = pynput.mouse.Button.left
 right = pynput.mouse.Button.right
+
+# NỘI SUY FPS => ĐỘ TRỄ
+# T_FPS = [
+#         [
+#             30,
+#             60,
+#             100,
+#             120,
+#             220],
+#         [
+#             1.5,
+#             1.366,
+#             1.34,
+#             1.32,
+#             1.285]]
+def fps2t(T_FPS, fps): #T_FPS: array, fps: float
+    """
+    Truy xuất giá trị t tương ứng từ bảng FPS bằng nội suy tuyến tính.
+
+    Parameters
+    ----------
+    T_FPS : list[list]
+        [
+            [fps1, fps2, fps3, ...],
+            [t1,   t2,   t3,   ...]
+        ]
+
+    fps : float
+
+    Returns
+    -------
+    float
+    """
+
+    fps_values = T_FPS[0]
+    t_values = T_FPS[1]
+
+    # FPS lớn hơn giá trị lớn nhất
+    if fps >= fps_values[-1]:
+        return t_values[-1]
+
+    # FPS nhỏ hơn giá trị nhỏ nhất
+    if fps <= fps_values[0]:
+        print("Tốc độ khung hình quá thấp để hỗ trợ")
+        return t_values[0]
+
+    # tìm đoạn chứa fps
+    for i in range(len(fps_values) - 1):
+
+        if fps_values[i] <= fps <= fps_values[i + 1]:
+
+            fps1 = fps_values[i]
+            fps2 = fps_values[i + 1]
+
+            t1 = t_values[i]
+            t2 = t_values[i + 1]
+
+            weight = (fps - fps1) / (fps2 - fps1)
+
+            return t1 + (t2 - t1) * weight
+
+    # fallback
+    return t_values[0]
+
+#n3w
+def skk3aw(fps):
+    start = time.perf_counter()
+
+    for _ in range(int(0.6 * fps)):
+        if time.perf_counter() - start > 0.6:
+            break
+
+        mouse.press(left)
+        mouse.release(left)
+        time.sleep(2 / fps)
+
+    inputkeyboard.press("w")
+
+    T_FPS = [
+        [30, 60, 100, 120, 220],
+        [1.5, 1.366, 1.34, 1.32, 1.285]
+    ]
+
+    t = fps2t(T_FPS, fps)
+
+    while time.perf_counter() - start < t:
+        pass
+
+    inputkeyboard.release("w")
+
+#n2d
+def skk2as(fps):
+    start = time.perf_counter()
+
+    for _ in range(int(0.26 * fps)):
+        if time.perf_counter() - start > 0.32:
+            break
+
+        mouse.press(left)
+        mouse.release(left)
+        time.sleep(2 / fps)
+
+    t = fps2t([
+        [60, 120, 220],
+        [0.44, 0.40, 0.37]
+    ], fps)
+
+    while time.perf_counter() - start < t:
+        pass
+
+    mouse.press(right)
+    time.sleep(1 / fps)
+    mouse.release(right)
+
+    time.sleep(1 / fps)
+    time.sleep(1 / fps)
+    time.sleep(1 / fps)
+
+    t = fps2t([
+        [60, 120, 220],
+        [0.63, 0.57, 0.56]
+    ], fps)
+
+    while time.perf_counter() - start < t:
+        pass
+
+#n2c
+def skk2az(fps):
+
+    start = time.perf_counter()
+
+    t = fps2t([
+        [60,120,220],
+        [0.24,0.22,0.20]
+    ], fps)
+
+    for _ in range(int(0.2 * fps)):
+        if time.perf_counter() - start > t:
+            break
+
+        mouse.press(left)
+        mouse.release(left)
+        time.sleep(2 / fps)
+
+    while time.perf_counter() - start < t + 2 / fps:
+        pass
+
+    mouse.press(left)
+    time.sleep(0.44)
+    mouse.release(left)
+
+    t = fps2t([
+        [60,120,220],
+        [1.16,1.12,1.09]
+    ], fps)
+
+    while time.perf_counter() - start < t:
+        pass
+
+
+def is_no_key_pressed():
+    keys = [
+        "caps lock",
+        "w",
+        "a",
+        "s",
+        "d",
+        "left",
+        "right"
+    ]
+
+    return not any(keyboard.is_pressed(key) for key in keys)
+
+#BLOCK 223
+def skk223_loop(fps):
+
+    start = time.perf_counter()
+
+    while time.perf_counter() - start < 20:
+
+        skk2as(fps)
+        skk2as(fps)
+        skk3aw(fps)
+
+        if is_no_key_pressed():
+            break
+
+
+#Xử lý code chạy macro
+running = False
+
+def worker():
+    global running
+
+    while running and keyboard.is_pressed("caps lock"):
+        skk223_loop(120)   # hoặc fps của bạn
+
+    running = False
+
+
+def on_press(event):
+    global running
+
+    if running:
+        return
+
+    running = True
+    threading.Thread(target=worker, daemon=True).start()
+
+
+def on_release(event):
+    global running
+    running = False
+
+
+keyboard.on_press_key("caps lock", on_press)
+keyboard.on_release_key("caps lock", on_release)
+
+keyboard.wait()
